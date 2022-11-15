6a56ec6d069218286ce7e939302c2aef9882ec7f
olafnub <googlhoogle@gmail.com>
2022-11-14 21:19:51 -0600

    Code javascript given in practices
    add index.html and script.js. Explain new added code from now on

---
+++ javascript.js
@@ -0,0 +1,173 @@
+let message="Hello!"; //store the string "Hello!" in the variable named message
+
+// alert(message);
+
+let user='John';
+let age=25;
+let messages='hello';
+
+// let user='john', age=25, messages='hello';
+
+let hello="Hello World!";
+let replacement = hello;
+// alert(replacement);
+
+let $ = 3;
+let _ = 1;
+
+// alert($ + _);
+
+const myBirthday = '12.05.2002';
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "0F0";
+const COLOR_BLUE = "00F";
+const COLOR_ORANGE = "#FF7F00";
+
+let color = COLOR_ORANGE;
+// alert(color);
+
+var admin;
+var name = "john";
+admin = name;
+// alert(admin);
+
+let planetEarth = "earth";
+let currentUser = "user";
+
+const myInt = 5;
+const myFloat = 6.77721312;
+// myInt;
+// myFloat;
+
+typeof myInt;
+typeof myFloat;
+
+// console.log(myFloat.toFixed(0));
+
+let myNumber = 74;
+while (myNumber < 100) {
+myNumber += 3;
+}
+// console.log(
+//     myNumber
+// );
+
+let myString = "74";
+myString= Number(myString);
+// console.log(myString + 2);
+
+let NUMBER_FOUR = 4;
+let NUMBER_FIFTY = 50;
+// console.log(11%5);
+// console.log(NUMBER_FIFTY!==50*3);
+let NUMBER_FIVE = 5;
+
+let answerKey = NUMBER_FIFTY = NUMBER_FIVE * 4;
+// console.log(answerKey);
+
+let numberOne, numberTwo, numberThree;
+numberTwo = 5;
+
+
+numberOne = numberTwo = numberThree = 2+2;
+// console.log(numberOne + " " + numberTwo);
+
+let q = 2;
+q = q + 5;
+q = q + 5;
+
+// console.log(q);
+
+let multiplyEqualsTwo = 2;
+// multiplyEqualsTwo *= 2+5;
+
+// console.log(multiplyEqualsTwo);
+// ++multiplyEqualsTwo;
+
+// console.log(multiplyEqualsTwo);
+
+let counter = ++multiplyEqualsTwo;
+
+// console.log(counter);
+
+counter = 3 * ++multiplyEqualsTwo;
+// alert(counter);
+counter++;
+
+for (a=1, b=3, c=a+b; a<10; a++) {
+    // console.log(a);
+}
+
+let alphabet = 1;
+
+let denominator = ++alphabet;
+
+// console.log(alphabet + " " + denominator);
+
+
+// 1. "10"
+// 2.  1
+// 3.  false , NaN (true + false = 1)
+// 4.  2
+// 5.  6
+// 6.  9px
+// 7.  $45
+// 8.  2
+// 9.  NaN
+// 10. -9 5
+// 11. NaN -14
+// 12. null
+// 13. undefined
+// 14. NaN
+
+// alert(\t);
+
+// let zimbwawe = prompt("First Number", 1);
+// let charqiti = prompt("Second Number", 2);
+
+// alert(+zimbwawe + +charqiti);
+
+let andrew = 10;
+// console.log(andrew);
+
+let beReal = 7 * andrew;
+// console.log(beReal);
+
+// console.log(19) / 77);
+
+let max = 57;
+let actual = max - 17;
+let percentage = actual / max;
+
+var newMessage = "42";
+// console.log(newMessage-2);
+
+if (true) {
+    var testVariable = true;
+
+}
+// alert(testVariable);
+
+for (var iAn = 0; iAn <= 10;) {
+    var one = 1;
+    iAn++;
+}
+// console.log(one);
+// console.log(iAn + " ");
+
+for (let iBn = 0; iBn <= 10; iBn++) {
+    // console.log(iBn);
+}
+
+function sayHi() {
+    if(true) {
+        var phrase = "Hello";
+    }
+    alert(phrase);
+}
+
+// sayHi();
+// alert(phrase); doesn't work because function (sayHi() wasn't called for)
+
+
