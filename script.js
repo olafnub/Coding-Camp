b842dba6e34cbd358aa1886702b35ec30881fa5f
olafnub <googlhoogle@gmail.com>
2022-12-08 16:32:20 -0600

    Polish website look with new index. and script.
    Reason - Wanted to make the website look more neat when you open it up.
    
    Resources used - https://stackoverflow.com/questions/908765/how-to-link-html-pages-in-same-or-different-folders

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-window.onload = function() {
-let runTimeAddition = 5+2;
-let returnRunTime = "expression equals " + runTimeAddition + " at the runtime";
-document.getElementById("run-time").innerHTML = returnRunTime;
-}
+// window.onload = function() {
+// let runTimeAddition = 5+2;
+// let returnRunTime = "expression equals " + runTimeAddition + " at the runtime";
+// document.getElementById("run-time").innerHTML = returnRunTime;
+// }
 // alert(addingRunTime);
