df42d8f33b2cf5db713a31872018b78a81a68fac
olafnub <googlhoogle@gmail.com>
2023-01-02 12:35:34 -0600

    Change military time clock to standard
    Why - Looks better if it's 0-12

--- script.js
+++ script.js
@@ -74,23 +74,48 @@ if (hours >= 0 && hours < 12) {
 } else {
     postMeridiem = 'p.m.'
 }
-currentTime = hours + ":" + minutes + " " + postMeridiem;
 
 // Change current hour to 0-12
 switch (hours) {
     case "13":
-        hours = 1;
+        hours = "1";
         break;
     case "14":
-        hours = 2;
+        hours = "2";
         break;
     case "15":
-        hours = 3;
+        hours = "3";
         break;
     case "16":
-        hours = 4;
+        hours = "4";
+        break;
+    case "17":
+        hours = "5";
+        break;
+    case "18":
+        hours = "6";
+        break;
+    case "19":
+        hours = "7";
+        break;
+    case "20":
+        hours = "8";
+        break;
+    case "21":
+        hours = "9";
+        break;
+    case "22":
+        hours = "10";
+        break;
+    case "23":
+        hours = "11";
+        break;
+    case "0":
+        hours = "12";
         break;
 }
+
+currentTime = hours + ":" + minutes + " " + postMeridiem;
 
 // Building the clock minute update
 setInterval(changeMinutes, 1000);
