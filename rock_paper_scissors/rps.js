7661b03f99d460034c1ca945b618450451400dd9
olafnub <googlhoogle@gmail.com>
2023-02-15 14:33:42 -0600

    Create ui for rps game

--- rock_paper_scissors/rps.js
+++ rock_paper_scissors/rps.js
@@ -5,21 +5,32 @@
 // Compare players choice to computer's choice
 // Tally the score
 
+let computerChoice = "";
+let choicesFrom = [];
+
 function getComputerChoice() {
     let rps = ["rock", "paper", "scissors"]
     let randomNumber = Math.floor(Math.random() * 3);
 
-    return rps[randomNumber];
+    computerChoice = rps[randomNumber];
+    choicesFrom.push(computerChoice);
 
 }
 
-function userChoice() {
-    let rps = prompt("Rock, Paper, Or Scissors");
+let btn = document.querySelectorAll('button');
 
-    return rps;
-}
+let user = "";
 
-let user = userChoice();
+Array.from(btn).forEach(button => {
+    let element = button.value;
+    button.addEventListener('click', () => {
+        user = element;
+        choicesFrom.push(element)
+        console.log(game());
+        gameOutput();
+    });
+});
+
 let computerScore = 0;
 let userScore = 0;
 let ties = 0;
@@ -55,14 +66,40 @@ function compareTwo(computer, user) {
 }
 
 function game() {
-    for (let i = 0; i < 5; i++) {
-        let computer = getComputerChoice();
-        compareTwo(computer, user);
+    getComputerChoice();
+    compareTwo(computerChoice, user);
+
+    if (computerScore >= 5) {
+        alert("computer wins!");
+        window.location.reload();
+        
+    } else if (userScore >= 5){
+        alert("You win!");    
+        window.location.reload();    
     }
+
     return "computerscore: " + computerScore + "\nyourscore: " + userScore +"\nties: " + ties;
 }
 
 function maxMin(max, min) {
     return Math.floor(Math.random() * (max-min+1))+min;
 }
-console.log(game());
+
+let rtn = document.querySelector('#return');
+let para = "";
+let txt = "";
+
+function gameOutput() {
+    choicesFrom.forEach(choice => {
+        para = document.createElement('p');
+        if (choice == user) {
+            txt = document.createTextNode(`Your Choice: ${choice}`);
+        } else {
+            txt = document.createTextNode(`Computer Choice: ${choice}`);
+        }
+        para.appendChild(txt);
+        rtn.appendChild(para);
+        choicesFrom = [];
+    })
+
+}
